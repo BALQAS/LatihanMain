188bb544b451292948e9ff15a39ed27b0cfc4e4d
BALQAS <101112610+BALQAS@users.noreply.github.com>
2022-03-12 02:31:11 +0700

    Implement funct Judul

---
+++ go.mod
@@ -0,0 +1,5 @@
+module latihanmain
+
+go 1.17
+
+require github.com/BALQAS/LatihanModule v0.0.0-20220311191741-17d46b5609a8 // indirect
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	latihanmodule "github.com/BALQAS/LatihanModule"
+)
+
+func main() {
+	latihanmodule.Judul("Brownies", "Coklat")
+	latihanmodule.CekBahan("telur")
+	fmt.Sprintln("")
+
+}
